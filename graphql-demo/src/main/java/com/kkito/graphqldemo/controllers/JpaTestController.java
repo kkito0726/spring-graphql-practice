6b8d5ef119fb3a4e95978b17a582652e64283724
kkito0726 <k.kito0726@gmail.com>
2023-05-29 14:53:00 +0000

    CRUDの実装

--- graphql-demo/src/main/java/com/kkito/graphqldemo/controllers/JpaTestController.java
+++ graphql-demo/src/main/java/com/kkito/graphqldemo/controllers/JpaTestController.java
@@ -1,16 +1,20 @@
 package com.kkito.graphqldemo.controllers;
 
 import com.kkito.graphqldemo.dto.CreateTodoDto;
+import com.kkito.graphqldemo.dto.UpdateTodoDto;
 import com.kkito.graphqldemo.models.Todo;
 import com.kkito.graphqldemo.repositories.TodoRepository;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +51,38 @@ public class JpaTestController {
     todoRepository.save(newTodo);
 
     return ResponseEntity.ok(newTodo);
+  }
+
+  @DeleteMapping("/{id}")
+  private ResponseEntity<Todo> deleteTodo(@PathVariable UUID id) {
+    Todo todo = _findTodoById(id);
+    todoRepository.delete(todo);
+
+    return ResponseEntity.ok(todo);
+  }
+
+  @PutMapping("/{id}")
+  private ResponseEntity<Todo> updateTodo(
+    @RequestBody UpdateTodoDto dto,
+    @PathVariable UUID id
+  ) {
+    Todo todo = _findTodoById(id);
+
+    if (Objects.nonNull(dto.getTitle())) {
+      todo.setTitle(dto.getTitle());
+    }
+
+    todoRepository.save(todo);
+
+    return ResponseEntity.ok(todo);
+  }
+
+  private Todo _findTodoById(UUID id) {
+    Optional<Todo> todo = todoRepository.findById(id);
+    if (todo.isEmpty()) {
+      return new Todo();
+    }
+
+    return todo.get();
   }
 }
