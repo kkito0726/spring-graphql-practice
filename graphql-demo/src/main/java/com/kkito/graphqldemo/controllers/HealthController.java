dcbef8f48b84af4a6aa7cfb5a26560e241b9c74d
kkito0726 <k.kito0726@gmail.com>
2023-05-30 09:46:55 +0000

    graphqlのhelloworldをやってみた

--- graphql-demo/src/main/java/com/kkito/graphqldemo/controllers/HealthController.java
+++ graphql-demo/src/main/java/com/kkito/graphqldemo/controllers/HealthController.java
@@ -1,5 +1,15 @@
 package com.kkito.graphqldemo.controllers;
 
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +24,35 @@ public class HealthController {
   @GetMapping
   public ResponseEntity<String> getHealth() {
     return ResponseEntity.ok("API is running");
+  }
+
+  @GetMapping("/graphql")
+  public ResponseEntity<String> helloGraphql() {
+    String schema = "type Query{hello: String}";
+
+    // 定義したスキーマを読み込む
+    SchemaParser schemaParser = new SchemaParser();
+    TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+    // Node.jsでやったときのResolver的な処理だと思われる
+    RuntimeWiring runtimeWiring = newRuntimeWiring()
+      .type(
+        "Query",
+        builder -> builder.dataFetcher("hello", new StaticDataFetcher("world"))
+      )
+      .build();
+
+    // TypeDefsとResolverを読み込んでgraphQLスキーマを生成
+    SchemaGenerator schemaGenerator = new SchemaGenerator();
+    GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(
+      typeDefinitionRegistry,
+      runtimeWiring
+    );
+
+    // GraphQLの実行
+    GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+    ExecutionResult executionResult = build.execute("{hello}");
+
+    return ResponseEntity.ok(executionResult.getData().toString());
   }
 }
